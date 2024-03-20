b00360c28182c386475913808920966a710a6732
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2024-03-20 09:59:12 -0300

    n

--- scriptttt.js
+++ scriptttt.js
@@ -1,36 +1,5 @@
-var agora = new Date()
+for( var c = 1;c <= 5;c++) {
 
-var diaSem = agora.getDay()
-
-/*
-Domingo - 0
-Sábado - 6
-*/
-
-console.log(diaSem)
-
-switch(diaSem) {
-
-case 0:
-
-console.log('Domingo')
-
-break
-
-case 1:
-
-console.log('Segunda')
-
-break
-
-case 2:
-
-console.log('Terça')
-
-break
-
-default:
-
-console.log('Dia Inválido')
-
-}
+    console.log(c)
+    
+    }
---
+++ src/javascript/teste.js
@@ -0,0 +1,8 @@
+var pessoa = {
+    nome: "João",
+    idade: 30,
+    cidade: "São Paulo"
+  }
+  
+  var jsonString = JSON.stringify(pessoa)
+  console.log(jsonString)
